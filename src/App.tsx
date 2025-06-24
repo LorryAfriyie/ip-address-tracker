c2114bd5ccd7b1c278e687fbb2e21d92bd80bd01
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-06-24 05:21:27 +0200

    Add IP geolocation fetch to App component
    Introduced a useEffect hook in App.tsx to fetch geolocation data for a sample IP address using the geo.ipify.org API and log the result. Also updated .gitignore to exclude .env files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <>
-    </>
-  )
+interface GeoData {
+  ip: string;
+  isp: string;
+  location: {
+    region: string;
+    timezone: string;
+  };
 }
 
-export default App
+function App() {
+  useEffect(() => {
+    fetch(
+      "https://geo.ipify.org/api/v2/country?apiKey=at_zJZXkPGIULduoxqxO9uqyPWe0CnAF&ipAddress=8.8.8.8"
+    )
+      .then((res) => res.json())
+      .then((data:GeoData) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return <></>;
+}
+
+export default App;
