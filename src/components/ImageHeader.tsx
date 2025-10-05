2784e38596a54781886216392d1e94784e1369d7
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-10-05 14:08:39 +0200

    CSS code refactor and image header changes
    Adding 100 view width to the header image to take up all of the horizontal space of the screen and changing location of boilerplate css code.

--- src/components/ImageHeader.tsx
+++ src/components/ImageHeader.tsx
@@ -3,12 +3,16 @@ import mobile_bg from "./pattern-bg-mobile.png";
 
 export default function ImageHeader() {
   return (
-    <div>
+    <section className={"image-header-section"}>
       <picture>
-        <source media={"(min-width: 40em)"} srcSet={desktop_bg} />
+        <source media={"(min-width: 60em)"} srcSet={desktop_bg} />
         <source srcSet={mobile_bg} />
-        <img src={desktop_bg} alt={desktop_bg} />
+        <img
+          src={desktop_bg}
+          alt={desktop_bg}
+          className={"image-header-section__img"}
+        />
       </picture>
-    </div>
+    </section>
   );
 }
