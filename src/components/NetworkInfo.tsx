06d428b25fd8753569810ad76701e18d615f4993
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-11-06 04:57:57 +0200

    Style NetworkInfo component and add SCSS module
    Refactored NetworkInfo.tsx to improve layout and add specific class names for styling. Introduced _NetworkInfo.scss for custom styles and updated _index.scss to forward the new SCSS module.

--- src/components/NetworkInfo.tsx
+++ src/components/NetworkInfo.tsx
@@ -10,27 +10,27 @@ export default function NetworkInfo({ ...props }: NetworkInfoProps) {
   return (
     <div
       className={
-        "flex justify-center items-center gap-5 bg-white w-100 rounded-[7px] dark-gray"
+        "flex justify-center items-center gap-5 bg-white rounded-[7px] dark-gray px-5 py-2 network-info"
       }
     >
       <div>
         <p className={"uppercase font-bold"}>ip address</p>
-        <p>{ip}</p>
+        <p className={"xl:text-2xl ip-address font-bold"}>{ip}</p>
       </div>
 
       <div>
         <p className={"uppercase font-bold"}>location</p>
-        <p>{region}</p>
+        <p className={"xl:text-2xl region font-bold"}>{region}</p>
       </div>
 
       <div>
         <p className={"uppercase font-bold"}>timezone</p>
-        <p>{timezone}</p>
+        <p className={"xl:text-2xl timezone font-bold"}>{timezone}</p>
       </div>
 
       <div>
         <p className={"uppercase font-bold"}>isp</p>
-        <p>{isp}</p>
+        <p className={"xl:text-2xl isp font-bold"}>{isp}</p>
       </div>
     </div>
   );
